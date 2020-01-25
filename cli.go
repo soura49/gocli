b4f5aa76b172a76d47f4ee3d826c420fd133c36d
soura49 <soura_balakrishna49@yahoo.com>
2020-01-25 13:27:12 -0700

    Add files via upload

---
+++ cli.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/choascli/commands"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	cmd := &cobra.Command{
+		Use:   "cli",
+		Short: "Welcome Time Series",
+	}
+	cmd.AddCommand(commands.PrintTime())
+	cmd.AddCommand(commands.PrintMonth())
+	cmd.AddCommand(commands.PrintDay())
+	cmd.AddCommand(commands.PrintZone())
+	err := cmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
